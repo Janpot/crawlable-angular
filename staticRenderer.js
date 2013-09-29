f81e27e89b993fdd153d6c36424cda21c4ea7e5e
Jan Potoms <potoms.jan@gmail.com>
2013-09-29 16:59:48 +0200

    Refactor and add cache example

--- staticRenderer.js
+++ staticRenderer.js
@@ -5,67 +5,75 @@
 
 var jsdom = require('jsdom');
 
+// basic caching
+var cache = {};
+
 module.exports = function init(config) {
   if (!config || typeof config.host !== 'string') {
     throw new Error('"config.host" is required.');
   }
   
   return function (req, res, next) {
-    
-    function processSnapshot(window) {
-      if (config.stripScripts) {
-        var scripts = window.document.querySelectorAll('script');
-        var i;
-        for (i = 0; i < scripts.length; i += 1) {
-          var script = scripts[i];
-          script.parentNode.removeChild(script);
-        }
-      }
-      // TO DO: cache this
-      var html = window.document.innerHTML;
-      res.send(200, html);
-    }
-    
     var path = req.query._escaped_fragment_;
     
-    if (path) {
-      var url = config.host + '/#!' + path;
-      var doc = jsdom.env({
-        url: url,
-        features: {
-          FetchExternalResources: ["script"],
-          ProcessExternalResources: ["script"]
-        },
-        done: function (errors, window) {
-          if (errors) {
-            // TO DO: analyze errors and send meaningful http response codes
-            res.send(500, errors);
-          } else {
-            
-            var timeout = setTimeout(function () {
-              processSnapshot(window);
-            }, config.timeout || 10000);
-            
-            window._onSnapshotReady = function (error) {
-              if (timeout) {
-                clearTimeout(timeout);
-                timeout = null;
-              }
-              
-              if (error) {
-                res.send(error);
-              } else {
-                processSnapshot(window);
-              }
-              
-              window._onSnapshotReady = null;
-            };
-            
-          }
-        }
-      });
-    } else {
+    if (!path) {
       next();
+      return;
     }
+      
+    var url = config.host + '/#!' + path;
+    
+    var cached = cache[url];
+    if (cached) {
+      res.send(200, cached);
+      return;
+    }
+    
+    var doc = jsdom.env({
+      url: url,
+      features: {
+        FetchExternalResources: ["script"],
+        ProcessExternalResources: ["script"]
+      },
+      done: function (errors, window) {
+        if (errors) {
+          res.send(500, errors);
+          return;
+        }
+          
+        var timeout = setTimeout(function () {
+          window._onSnapshotReady = null;
+          res.send(504);
+        }, config.timeout || 10000);
+        
+        window._onSnapshotReady = function (error) {
+          window._onSnapshotReady = null;
+          
+          if (timeout) {
+            clearTimeout(timeout);
+            timeout = null;
+          }
+          
+          if (error) {
+            res.send(error);
+            return;
+          }
+          
+          if (config.stripScripts) {
+            var scripts = window.document.querySelectorAll('script');
+            var i;
+            for (i = 0; i < scripts.length; i += 1) {
+              var script = scripts[i];
+              script.parentNode.removeChild(script);
+            }
+          }
+          
+          var html = window.document.innerHTML;
+          cache[url] = html;
+          res.send(200, html);
+        };
+        
+      }
+    });
   };
 };
